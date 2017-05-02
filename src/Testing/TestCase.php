776fb98ab087893a679c8b561b809633f0a2c4a9
Christian Persson <christian@infobaleen.com>
2017-05-02 14:39:20 +0200

    Added support for mysql database connection for testing purposes

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -5,6 +5,8 @@ use Larastart\Providers\LarastartServiceProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    protected $db = 'sqlite';
+
     /**
      * Setup the test environment.
      */
@@ -18,12 +20,24 @@ class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function getEnvironmentSetUp($app)
     {
-        $app['config']->set('database.default', 'testing');
-        $app['config']->set('database.connections.testing', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
+        if ($this->db === 'sqlite') {
+            $app['config']->set('database.default', 'testing');
+            $app['config']->set('database.connections.testing', [
+                'driver' => 'sqlite',
+                'database' => ':memory:',
+                'prefix' => '',
+            ]);
+        } else if ($this->db === 'mysql') {
+            $app['config']->set('database.default', 'mysql');
+            $app['config']->set('database.connections.testing', [
+                'driver' => 'mysql',
+                'username' => 'homestead',
+                'host' => '127.0.0.1',
+                'port' => '33061',
+                'database' => 'phpunit',
+                'password' => 'secret'
+            ]);
+        }
     }
 
     protected function getPackageProviders($app)
