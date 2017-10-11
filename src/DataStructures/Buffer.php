3eaf2cd723a6d8bfc474b6973f6eaa134d8a6eaf
Christian Persson <christian@infobaleen.com>
2017-10-11 09:12:38 +0200

    Added buffer class

---
+++ src/DataStructures/Buffer.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Larastart\DataStructures;
+
+
+class Buffer
+{
+    private $maxSize;
+    private $cb;
+
+    private $buffer = [];
+    private $bufferSize = 0;
+
+    public function __construct($maxSize, callable $cb)
+    {
+        $this->maxSize = $maxSize;
+        $this->cb = $cb;
+    }
+
+    public function add($el)
+    {
+        $this->buffer[] = $el;
+        if ($this->bufferSize++ > $this->maxSize) {
+            $this->finalize();
+        }
+    }
+
+    public function finalize()
+    {
+        call_user_func($this->cb, $this->buffer);
+        $this->buffer = [];
+        $this->bufferSize = 0;
+    }
+
+    public function __destruct()
+    {
+        if ($this->bufferSize > 0) {
+            \Log::warning("Buffer size > 0 in destructor..");
+        }
+    }
+
+}
