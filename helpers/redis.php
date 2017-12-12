eef7a0aca291c8c3443a1b6cb5b79b17ea7bca75
Christian Persson <christian@infobaleen.com>
2017-12-12 12:41:14 +0100

    Add redis_mutex_promise

--- helpers/redis.php
+++ helpers/redis.php
@@ -1,30 +1,69 @@
 <?php
 
 
+function redis_encode_value($res)
+{
+    return json_encode(["value" => $res, "created_at" => time()]);
+}
+
+function redis_decode_value($string)
+{
+    $obj = json_decode($string, 1);
+    return $obj["value"];
+}
+
+function redis_mutex_promise($key, callable $f, $max_wait_seconds = 10, $result_valid_seconds = 60, $max_block_time = 60)
+{
+    $data_key = 'redis_mutex_promise_data:' . $key;
+    $lock_key = 'redis_mutex_promise_lock:' . $key;
+    $max_ts = $max_wait_seconds + time();
+
+    do {
+        if (($res = Redis::get($data_key))) {
+            return redis_decode_value($res);
+        }
+        if (Redis::setNx($lock_key, time())) {
+            Redis::expire($lock_key, $max_block_time);
+            try {
+                $res = $f();
+                Redis::setEx($data_key, $result_valid_seconds, redis_encode_value($res));
+                Redis::del($lock_key);
+                return $res;
+            } catch (\Exception $e) {
+                Redis::del($lock_key);
+                return false;
+            }
+        }
+    } while (time() <= $max_ts);
+    return false;
+}
+
+
 function redis_mutex_cb($key, callable $f, $sleep = 10, $max_block_seconds = 60 * 60)
 {
-    $key = 'mutex_cb:' . $key;
+    $key = 'redis_mutex_cb:' . $key;
     $max_block_seconds = max($sleep + 1, $max_block_seconds);
     $max_blocked_to = time() + $max_block_seconds;
 
-    if (Redis::setNx($key, 'dummy') === 1) {
+    if (Redis::setNx($key, time()) === 1) {
         Redis::expire($key, $max_block_seconds);
         try {
             $f();
         } catch (\Exception $exception) {
-            echo "failed\n";
+//            echo "failed\n";
         }
         if (time() >= $max_blocked_to) {
-            echo "we passed max block seconds... Release the lock without sleep." . PHP_EOL;
+//            echo "we passed max block seconds... Release the lock without sleep." . PHP_EOL;
         } else {
             $ttl = min($max_blocked_to, time() + $sleep) - time();
             Redis::expire($key, $ttl);
-            echo "update ttl for key=$key to $ttl seconds" . PHP_EOL;
+//            echo "update ttl for key=$key to $ttl seconds" . PHP_EOL;
         }
     } else {
-        echo "Blocked in " . Redis::ttl($key) . " seconds" . PHP_EOL;
+//        echo "Blocked in " . Redis::ttl($key) . " seconds" . PHP_EOL;
     }
 }
+
 
 //https://stackoverflow.com/questions/4006324/how-to-atomically-delete-keys-matching-a-pattern-using-redis
 function redis_del_prefix($prefix)
