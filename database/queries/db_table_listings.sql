cacbe727270f91c51c98c978a871a11a0f2d59a2
Christian Persson <christian@infobaleen.com>
2018-11-14 20:08:47 +0100

    Add alias to db_table_listings

--- database/queries/db_table_listings.sql
+++ database/queries/db_table_listings.sql
@@ -1,12 +1,12 @@
 SELECT
-  table_schema,
-  table_name,
-  engine,
-  table_rows,
+  table_schema table_schema,
+  table_name table_name,
+  engine engine,
+  table_rows table_rows,
   round(data_length / 1024)                  data_length,
   round(index_length / 1024)                 index_length,
   round((data_length + index_length) / 1024) total_length,
-  table_collation
+  table_collation table_collation
 FROM information_schema.tables
 WHERE table_schema = :db
 ORDER BY total_length DESC
